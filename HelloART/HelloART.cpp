21b52b157247aaee9560f43c230037f549d3f475
Tuwuh Sarwoprasojo <tuwuh@formulatrix.com>
2016-11-14 02:18:04 +0700

    Detect and draw matrix marker

--- HelloART/HelloART.cpp
+++ HelloART/HelloART.cpp
@@ -22,6 +22,35 @@ static void Display()
 	arglPixelBufferDataUpload(g_pArglSettings, g_pARTImage);
 	arglDispImage(g_pArglSettings);
 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, (GLdouble)g_pARHandle->xsize, 0, (GLdouble)g_pARHandle->ysize, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glDisable(GL_LIGHTING);
+	glDisable(GL_DEPTH_TEST);
+
+	GLfloat vertices[6][2];
+	glLoadIdentity();
+	glVertexPointer(2, GL_FLOAT, 0, vertices);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glLineWidth(2.0f);
+	for (int j = 0; j < g_pARHandle->marker_num; j++) {
+		ARMarkerInfo* pMarker = &(g_pARHandle->markerInfo[j]);
+		if (pMarker->cutoffPhase != AR_MARKER_INFO_CUTOFF_PHASE_NONE) {
+			continue;
+		}
+		int i;
+		for (i = 0; i < 5; i++) {
+			int dir = pMarker->dir;
+			vertices[i][0] = (float)pMarker->vertex[(i + 4 - dir) % 4][0];
+			vertices[i][1] = ((float)g_pARHandle->ysize - (float)pMarker->vertex[(i + 4 - dir) % 4][1]);
+		}
+		vertices[i][0] = (float)pMarker->pos[0];
+		vertices[i][1] = ((float)g_pARHandle->ysize - (float)pMarker->pos[1]);
+		glDrawArrays(GL_LINE_STRIP, 0, 6);
+	}
+	glDisableClientState(GL_VERTEX_ARRAY);
+
 	glutSwapBuffers();
 }
 
@@ -32,6 +61,7 @@ static void MainLoop()
 	pImage = arVideoGetImage();
 	if (pImage) {
 		g_pARTImage = pImage;
+		arDetectMarker(g_pARHandle, g_pARTImage);
 		glutPostRedisplay();
 	}
 }
@@ -62,7 +92,15 @@ int main(int argc, char* argv[])
 
 	g_pARHandle = arCreateHandle(pCparamLT);
 	arSetPixelFormat(g_pARHandle, pixformat);
+	arSetDebugMode(g_pARHandle, AR_DEBUG_DISABLE);
 	arVideoCapStart();
+
+	// Setup AR
+	//arSetPatternDetectionMode(g_pARHandle, AR_DEFAULT_PATTERN_DETECTION_MODE);
+	arSetPatternDetectionMode(g_pARHandle, AR_MATRIX_CODE_DETECTION);
+	arSetLabelingMode(g_pARHandle, AR_DEFAULT_LABELING_MODE);
+	arSetPattRatio(g_pARHandle, AR_PATT_RATIO);
+	arSetMatrixCodeType(g_pARHandle, AR_MATRIX_CODE_TYPE_DEFAULT);
 
 	// Setup GLUT and gsub_lite
 	glutInit(&argc, argv);
@@ -72,13 +110,15 @@ int main(int argc, char* argv[])
 
 	g_pArglSettings = arglSetupForCurrentContext(&(pCparamLT->param), pixformat);
 	arglSetupDebugMode(g_pArglSettings, g_pARHandle);
-	arUtilTimerReset();
 
+	// Register GLUT functions
 	glutDisplayFunc(Display);
 	glutIdleFunc(MainLoop);
 
+	// The main loop
 	glutMainLoop();
 
+	// Clean up
 	arDeleteHandle(g_pARHandle);
 
 	return 0;
