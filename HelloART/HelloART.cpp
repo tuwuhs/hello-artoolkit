5fce913f685b1bd267cbc12b15bec1496b4353ae
Tuwuh Sarwoprasojo <tuwuh@formulatrix.com>
2016-11-14 00:20:45 +0700

    Create AR handle, use default camera params

--- HelloART/HelloART.cpp
+++ HelloART/HelloART.cpp
@@ -4,7 +4,10 @@
 #include "stdafx.h"
 
 #include <AR/ar.h>
+#include <AR/video.h>
 #include <stdio.h>
+
+static ARHandle* g_pARHandle;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -12,6 +15,20 @@ int _tmain(int argc, _TCHAR* argv[])
 	arGetVersion(&pVersion);
 	printf("Hello ARToolKit ver. %s\r\n", pVersion);
 	free(pVersion);
+
+	//arVideoOpen("-device=Image");
+	
+	ARParam cparam;
+	arParamLoad("camera_para.dat", 1, &cparam);
+	printf("cparam xsize: %d ysize: %d\r\n", cparam.xsize, cparam.ysize);
+	// TODO: need to set size according to input file
+
+	ARParamLT cparamLT;
+	arParamLTCreate(&cparam, AR_PARAM_LT_DEFAULT_OFFSET);
+
+	g_pARHandle = arCreateHandle(&cparamLT);
+
+	arDeleteHandle(g_pARHandle);
 
 	return 0;
 }
